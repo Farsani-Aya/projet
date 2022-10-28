2583ff576a0e27b1e212b894973c25f238de4cc8
Farsani-Aya <116493067+Farsani-Aya@users.noreply.github.com>
2022-10-28 12:44:17 +0200

    Add files via upload

---
+++ date.js
@@ -0,0 +1,12 @@
+function time(){
+    var form;
+    date= new Date();
+    hours= date.getHours();
+    minute= date.getMinutes();
+    sec= date.getSeconds();
+    form =hours+':'+minute+':'+sec;
+    document.getElementById("clock").innerText=form;
+    setTimeout(time, 1000);
+
+}
+time();
